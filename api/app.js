17348f05be90382adfb2c1a5d19c8de63a9f66a5
mvincentmiller <m.vincent83@gmail.com>
2020-04-03 14:15:01 -0500

    postgraphile

--- api/app.js
+++ api/app.js
@@ -1,16 +1,33 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('queue_database', 'queue', 'queue', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 9,
-    min: 0,
-    idle: 10000
-  }
-});
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('queue_database', 'queue', 'queue', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   pool: {
+//     max: 9,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
-sequelize.authenticate().then(() => {
-  console.log("Success!");
-}).catch((err) => {
-  console.log(err);
-});
+// sequelize.authenticate().then(() => {
+//   console.log("Success!");
+// }).catch((err) => {
+//   console.log(err);
+// });
+
+const http = require("http");
+const { postgraphile } = require("postgraphile");
+
+http
+  .createServer(
+    postgraphile(
+      process.env.DATABASE_URL || "postgres://queue:queue@localhost:5432/queue_database",
+      "public",
+      {
+        watchPg: true,
+        graphiql: true,
+        enhanceGraphiql: true,
+      }
+    )
+  )
+  .listen(process.env.PORT || 3000);
