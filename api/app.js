821d121c1c574706e6efcfd3e4163c7d1fe96104
mvincentmiller <m.vincent83@gmail.com>
2020-04-03 13:50:58 -0500

    hooking into db, docker-compose

--- api/app.js
+++ api/app.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('queue', 'postgres', 'queue', {
+const sequelize = new Sequelize('queue_database', 'queue', 'queue', {
   host: 'localhost',
   dialect: 'postgres',
   pool: {
